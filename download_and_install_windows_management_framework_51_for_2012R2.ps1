09d2d275b3bbc075ec26b59bebeabaa36a771fa4
akbaraziz <akbar.aziz@nutanix.com>
2019-12-07 11:46:41 -0600

    Updated Scripts

--- download_and_install_windows_management_framework_51_for_2012R2.ps1
+++ download_and_install_windows_management_framework_51_for_2012R2.ps1
@@ -3,3 +3,9 @@ Invoke-WebRequest -Uri "https://download.microsoft.com/download/6/F/5/6F5FF66C-6
 
 # Install Windows Management Framework 5.1 on Windows 2012 R2
 Start-Process -Filepath "$PSScriptRoot/Win8.1AndW2K12R2-KB3191564-x64.msu" -ArgumentList /quiet /norestart
+
+# Remove Installer When Finished
+Remove-Item -recurse "C:\Win8.1AndW2K12R2-KB3191564-x64.msu"
+
+# Reboot System
+Restart-Computer
