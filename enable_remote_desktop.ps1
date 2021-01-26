82ddde3897ea221ead07b02ef46b3b2e50b4bed1
akbaraziz <akbar.aziz@nutanix.com>
2021-01-25 22:06:03 -0600

    Updated Scripts

--- enable_remote_desktop.ps1
+++ enable_remote_desktop.ps1
@@ -1,5 +1,5 @@
 # Enable Windows Remote Desktop
-Invoke-Command -Computername <computer name> -ScriptBlock {Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 0 }
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 
 # Add Firewall Rule
-Invoke-Command -Computername <computer name> -ScriptBlock {Enable-NetFirewallRule -DisplayGroup "Remote Desktop"}
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
